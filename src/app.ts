c167a9a176b195baf4547ab760c1a4c9dbf0ead9
elechen <elechen@outlook.com>
2018-12-29 15:02:55 +0800

    rename to mp

--- src/app.ts
+++ src/app.ts
@@ -5,12 +5,14 @@ import { router as authtoken } from '@api/authtoken';
 import { router as textmsg } from '@api/textmsg';
 
 const app: express.Application = express();
-if(process.env.WX_TOKEN){
-  app.use('/wechat', authtoken);
-}else{
-  app.use('/wechat', textmsg);
+if (process.env.WX_TOKEN) {
+  app.use('/mp', authtoken);
+  console.log('now is wechat auth token mode');
+} else {
+  app.use('/mp', textmsg);
+  console.log('now is wechat normal mode');
 }
 
 app.listen(8000, function () {
-  console.log('app is running on port 8000', process.env.WX_TOKEN);
+  console.log('app is running on port:8000');
 })
