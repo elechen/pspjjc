aadda0a236011fe5eff537beacff66211ed310fe
elechen <elechen@outlook.com>
2018-12-29 18:07:08 +0800

    add get handler

--- src/api/mp/mp.ts
+++ src/api/mp/mp.ts
@@ -7,8 +7,8 @@ import * as subscribe from '@api/mp/subscribe';
 
 export let router = Router();
 router.post('/', xmlparser({ trim: false, explicitArray: false }), function (req: Request, res: Response) {
-  console.log('args->', req.body);
-  let handler = GetHandler(req);
+  console.log('body->', req.body);
+  let handler = PosHandler(req);
   if (handler) {
     handler(req, res);
   } else {
@@ -16,11 +16,27 @@ router.post('/', xmlparser({ trim: false, explicitArray: false }), function (req
   }
 });
 
+router.get('/', function (req:Request, res:Response){
+  console.log('query->', req.query);
+  let handler = GetHandler(req);
+  if (handler) {
+    handler(req, res);
+  } else {
+    res.send('success');
+  }
+});
+
 function GetHandler(req: Request): (req: Request, res: Response) => void {
   let func;
-  if (req.query['echostr']) {
+  if (req.query.echostr) {
     func = authtoken.handler;
-  } else if (req.body.xml['msgtype'] == 'text') {
+  }
+  return func;
+}
+
+function PosHandler(req: Request): (req: Request, res: Response) => void {
+  let func;
+  if (req.body.xml['msgtype'] == 'text') {
     func = textmsg.handler;
   } else if (req.body.xml['event'] == 'subscribe') {
     func = subscribe.handler;
